5d1562a3fa605741f86b842ec8b749b0b7d17b61
muralidharan-1708 <muralidharan1708@gmail.com>
2024-11-26 09:30:56 +0530

    imported time for the execution

--- poppler/pdf_to_img.py
+++ poppler/pdf_to_img.py
@@ -1,5 +1,12 @@
+import time
 from pdf2image import convert_from_path
 
-image = convert_from_path('NPTEL_2024.pdf', 500,poppler_path=r'D:\Program Files\poppler-24.08.0\Library\bin')
+start_time = time.time()
+
+image = convert_from_path(r"C:\Users\MuraliDharan S\OneDrive\Documents\OCR_extraction.pdf", 500, poppler_path=r'D:\Program Files\poppler-24.08.0\Library\bin')
 for i in range(len(image)):
     image[i].save('page'+str(i)+'.jpg', 'JPEG')
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time} seconds")
